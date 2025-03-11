185c98ed4db03de5b184636930d10aaadc699037
yyyyoshida <thth331111@gmail.com>
2025-03-11 10:51:05 +0900

    軽量化して読み込み遅延を軽減

--- src/components/TrackInfo.js
+++ src/components/TrackInfo.js
@@ -5,60 +5,73 @@ import { usePlayerContext } from './PlayerContext';
 const TrackInfo = () => {
   const [title, setTitle] = useState('曲のタイトル');
   const [artist, setArtist] = useState('アーティスト・作者');
-  // const [thumbnail, setThumbnail] = useState('');
   const imgRef = useRef(null);
-  // const [isHidden, setIsHidden] = useState('visible');
+  const [isHidden, setIsHidden] = useState('visible');
   const { isPlaying, currentSongIndex } = usePlayerContext();
   const isFirstRender = useRef(true);
+  const transitionRef = useRef(null);
 
-  // useEffect(() => {
-  //   if (isFirstRender.current) {
-  //     return;
-  //   }
-  //   setIsHidden('hidden');
-  // }, [isPlaying, currentSongIndex]);
+  function fadeTransition() {
+    const transitionElement = transitionRef.current;
+    transitionElement.style.visibility = 'visible';
+    transitionElement.style.opacity = 1;
+    function handleTransitionEnd() {
+      transitionElement.style.visibility = 'hidden';
+      transitionElement.style.opacity = 1;
+    }
+    setTimeout(() => {
+      transitionElement.style.opacity = 0;
+      transitionElement.addEventListener('transitionend', handleTransitionEnd);
+    }, 50);
+    // }, 100);
+  }
+
+  useEffect(() => {
+    if (isFirstRender.current) {
+      return;
+    }
+    setIsHidden('hidden');
+  }, [isPlaying, currentSongIndex]);
 
   useEffect(() => {
     if (isFirstRender.current) {
       isFirstRender.current = false;
       return;
     }
-
+    console.log('２回目のレンダリング');
     setTitle(songs[currentSongIndex].title);
     setArtist(songs[currentSongIndex].artist);
     imgRef.current.src = songs[currentSongIndex].cover;
   }, [currentSongIndex, isPlaying]);
+
+  useEffect(() => {
+    fadeTransition();
+  }, [currentSongIndex]);
 
   return (
     <>
       <div id="js-track-info" className="player-controls__track-info">
         <figure className="player-controls__track">
           <div id="js-track-thumbnail-wrapper" className="player-controls__track-thumbnail-wrapper">
-            {/* <img id="js-track-thumbnail" src="img/写真アイコン4.png" alt="サムネイル" className="player-controls__track-thumbnail" /> */}
             <img
               ref={imgRef}
               id="js-track-thumbnail"
               src={'img/写真アイコン4.png'}
-              // src={thumbnail || 'img/写真アイコン4.png'}
-              // src={`${thumbnail}`}
               alt="サムネイル"
-              // alt={title}
               className="player-controls__track-thumbnail"
             />
             <div
+              ref={transitionRef}
               id="js-track-thumbnail-transition"
               className="player-controls__track-thumbnail-transition"
               // style={{ visibility: isHidden ? 'visible' : 'hidden' }}
               // style={{ visibility: isHidden ? 'hidden' : 'visible' }}
-              // style={{ visibility: isHidden }}
-              style={{ visibility: 'hidden' }}
+              style={{ visibility: isHidden }}
+              // style={{ visibility: 'hidden' }}
             ></div>
           </div>
           <figcaption id="js-track-meta" className="player-controls__track-meta">
-            {/* 根本的解決にはならないが、最初に表示されるバグを一次的にこれで解消↓↓ */}
-            {/* <p style={{ visibility: isHidden ? 'hidden' : 'visible' }} className="player-controls__title"> */}
             <p className="player-controls__title">{title}</p>
-            {/* <p style={{ visibility: isHidden ? 'hidden' : 'visible' }} className="player-controls__artist"> */}
             <p className="player-controls__artist">{artist}</p>
           </figcaption>
         </figure>
