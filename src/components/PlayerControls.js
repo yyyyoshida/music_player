acf5ecf88465eb1af3032b4dcd6ae8711cc50315
yyyyoshida <thth331111@gmail.com>
2025-03-18 20:59:06 +0900

    トラック情報をクリック時フェードイン・フェードアウト追加 (#3)

--- src/components/PlayerControls.js
+++ src/components/PlayerControls.js
@@ -1,27 +1,30 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useContext } from 'react';
 import ProgressBar from './ProgressBar';
 import TrackInfo from './TrackInfo';
 import PlayerActions from './PlayerActions';
 import PlayerOptions from './PlayerOptions';
-import { PlayerProvider } from './PlayerContext';
+// import { PlayerProvider } from './PlayerContext';
 import { RepeatProvider } from './RepeatContext';
+import { TrackInfoContext } from './TrackInfoContext';
 
 const PlayerControls = () => {
   const actionsRef = useRef(null);
+  const { isVisible } = useContext(TrackInfoContext);
 
   return (
-    <div className="player-controls">
+    // <div className="player-controls">
+    <div className={`player-controls ${isVisible ? 'thumbnail-expanded has-overlay' : ''}`}>
       {/* isPlayingを使いまわすため */}
-      <PlayerProvider>
-        <RepeatProvider>
-          <ProgressBar />
-          <div className="player-controls__info">
-            <TrackInfo actionsRef={actionsRef} />
-            <PlayerActions actionsRef={actionsRef} />
-            <PlayerOptions />
-          </div>
-        </RepeatProvider>
-      </PlayerProvider>
+      {/* <PlayerProvider> */}
+      <RepeatProvider>
+        <ProgressBar />
+        <div className="player-controls__info">
+          <TrackInfo actionsRef={actionsRef} />
+          <PlayerActions actionsRef={actionsRef} />
+          <PlayerOptions />
+        </div>
+      </RepeatProvider>
+      {/* </PlayerProvider> */}
     </div>
   );
 };
--- src/components/ThumbnailPreview.js
+++ src/components/ThumbnailPreview.js
@@ -1,20 +1,75 @@
-import React from 'react';
+import React, { useContext, useRef, useState, useEffect } from 'react';
+import { TrackInfoContext } from './TrackInfoContext';
+import { songs } from './PlayMusic';
+import { usePlayerContext } from './PlayerContext';
 
 export const ThumbnailPreview = () => {
+  const { isVisible } = useContext(TrackInfoContext);
+  const thumbnailPreviewRef = useRef(null);
+  const [delayedVisibility, setDelayedVisibility] = useState('hidden');
+  const { isPlaying, currentSongIndex } = usePlayerContext();
+  const [title, setTitle] = useState('曲がセットされていません。');
+  const [artist, setArtist] = useState('');
+  const coverArtRef = useRef(null);
+  const transitionRef = useRef(null);
+
+  function showThumbnail() {
+    setDelayedVisibility('visible');
+  }
+
+  function hideThumbnailDelay() {
+    const timer = setTimeout(() => {
+      setDelayedVisibility('hidden');
+    }, 300);
+    return () => clearTimeout(timer);
+  }
+  // TrackInfoの中のfadeTransition関数も同じだから今後使い回せるようにする。↓↓
+  function fadeTransition() {
+    const transitionElement = transitionRef.current;
+    transitionElement.style.visibility = 'visible';
+    transitionElement.style.opacity = 1;
+    function handleTransitionEnd() {
+      transitionElement.style.visibility = 'hidden';
+      transitionElement.style.opacity = 1;
+    }
+    setTimeout(() => {
+      transitionElement.style.opacity = 0;
+      transitionElement.addEventListener('transitionend', handleTransitionEnd);
+    }, 50);
+  }
+
+  useEffect(() => {
+    if (isVisible) {
+      showThumbnail();
+    } else {
+      hideThumbnailDelay();
+    }
+  }, [isVisible]);
+
+  useEffect(() => {
+    setTitle(songs[currentSongIndex].title);
+    setArtist(songs[currentSongIndex].artist);
+    coverArtRef.current.src = songs[currentSongIndex].cover;
+
+    fadeTransition();
+  }, [currentSongIndex]);
+
   return (
     <>
-      <div id="js-thumbnail-preview" className="thumbnail-preview">
-        <div id="js-thumbnail-preview-background" className="thumbnail-preview__background"></div>
+      <div
+        ref={thumbnailPreviewRef}
+        className={`thumbnail-preview ${isVisible ? 'is-visible' : ''}`}
+        style={{ opacity: isVisible ? 1 : 0, visibility: delayedVisibility }}
+      >
+        <div className="thumbnail-preview__background" style={{ backgroundImage: `url(${songs[currentSongIndex].cover})` }}></div>
         <figure className="thumbnail-preview__content">
           <div className="thumbnail-preview__image-warpper">
-            <img id="js-thumbnail-preview-image" className="thumbnail-preview__image" src="img/not-found.jpg" alt="" />
-            <div id="js-thumbnail-preview-transition" className="thumbnail-preview__image-transition"></div>
+            <img ref={coverArtRef} className="thumbnail-preview__image" src="img/not-found.jpg" alt="" />
+            <div ref={transitionRef} className="thumbnail-preview__image-transition"></div>
           </div>
           <figcaption className="thumbnail-preview__info">
-            <p id="js-thumbnail-preview-title" className="thumbnail-preview__title">
-              曲がセットされていません
-            </p>
-            <p id="js-thumbnail-preview-artist" className="thumbnail-preview__artist"></p>
+            <p className="thumbnail-preview__title">{title}</p>
+            <p className="thumbnail-preview__artist">{artist}</p>
           </figcaption>
         </figure>
       </div>
