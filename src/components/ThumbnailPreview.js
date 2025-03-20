3be32b54b85d9e364b94e12179a3818247c76faa
yyyyoshida <thth331111@gmail.com>
2025-03-20 17:48:43 +0900

    リファクタリング (#11)

--- src/components/ThumbnailPreview.js
+++ src/components/ThumbnailPreview.js
@@ -1,34 +1,41 @@
 import React, { useContext, useRef, useState, useEffect } from 'react';
 import { TrackInfoContext } from './TrackInfoContext';
 import { songs, music } from './PlayMusic';
-// import { songs } from '../components/PlayMusic';
 import { usePlayerContext } from './PlayerContext';
-import { useAudioVisualizer } from '../hooks/useAudioVisualizer';
 
 export const ThumbnailPreview = () => {
   const { isVisible } = useContext(TrackInfoContext);
-  const thumbnailPreviewRef = useRef(null);
+  const { currentSongIndex } = usePlayerContext();
+
   const [delayedVisibility, setDelayedVisibility] = useState('hidden');
-  const { isPlaying, currentSongIndex } = usePlayerContext();
   const [title, setTitle] = useState('曲がセットされていません。');
   const [artist, setArtist] = useState('');
+  const [scale, setScale] = useState(1);
+
+  const thumbnailPreviewRef = useRef(null);
   const coverArtRef = useRef(null);
   const transitionRef = useRef(null);
-  const scale = useAudioVisualizer();
-  // const [scale, setScale] = useState(1);
+  const audioContextRef = useRef(null);
+  const analyserRef = useRef(null);
+  const sourceRef = useRef(null);
+  const dataArrayRef = useRef(null);
 
   function showThumbnail() {
     setDelayedVisibility('visible');
   }
 
   function hideThumbnailDelay() {
+    const hideDelay = 300;
     const timer = setTimeout(() => {
       setDelayedVisibility('hidden');
-    }, 300);
+    }, hideDelay);
     return () => clearTimeout(timer);
   }
+
   // TrackInfoの中のfadeTransition関数も同じだから今後使い回せるようにする。↓↓
   function fadeTransition() {
+    // const transitionDelay = 500; // たまにtransitionのアニメーションが途切れる
+    const transitionDelay = 50;
     const transitionElement = transitionRef.current;
     transitionElement.style.visibility = 'visible';
     transitionElement.style.opacity = 1;
@@ -39,7 +46,7 @@ export const ThumbnailPreview = () => {
     setTimeout(() => {
       transitionElement.style.opacity = 0;
       transitionElement.addEventListener('transitionend', handleTransitionEnd);
-    }, 50);
+    }, transitionDelay);
   }
 
   useEffect(() => {
@@ -58,32 +65,52 @@ export const ThumbnailPreview = () => {
     fadeTransition();
   }, [currentSongIndex]);
 
-  // const audioContext = new (window.AudioContext || window.webkitAudioContext)();
-  // const analyser = audioContext.createAnalyser();
-  // const source = audioContext.createMediaElementSource(music);
-  // source.connect(analyser);
-  // analyser.connect(audioContext.destination);
-  // analyser.fftSize = 256; // 周波数の解像度
-  // const bufferLength = analyser.frequencyBinCount;
-  // const dataArray = new Uint8Array(bufferLength);
+  // 曲に合わせて音楽の画像が動的に変化////////////////////////////////////////////
 
-  // function updateImageSize() {
-  //   analyser.getByteFrequencyData(dataArray);
+  function setupAudioContext() {
+    if (!audioContextRef.current) {
+      audioContextRef.current = new (window.AudioContext || window.webkitAudioContext)();
+      analyserRef.current = audioContextRef.current.createAnalyser();
+      analyserRef.current.fftSize = 256;
+      dataArrayRef.current = new Uint8Array(analyserRef.current.frequencyBinCount);
+    }
+  }
 
-  //   let sum = 0;
-  //   for (let i = 0; i < bufferLength; i++) {
-  //     sum += dataArray[i];
-  //   }
-  //   const average = sum / bufferLength;
-  //   // const scale = 1 + average / 800;
-  //   setScale(1 + average / 800);
-  //   requestAnimationFrame(updateImageSize);
-  // }
-  // music.onplay = () => {
-  //   audioContext.resume().then(() => {
-  //     updateImageSize();
-  //   });
-  // };
+  function setupAudioSource() {
+    if (!sourceRef.current) {
+      sourceRef.current = audioContextRef.current.createMediaElementSource(music);
+      sourceRef.current.connect(analyserRef.current);
+      analyserRef.current.connect(audioContextRef.current.destination);
+    }
+  }
+
+  function startAudioVisualization() {
+    music.onplay = () => {
+      audioContextRef.current.resume().then(() => {
+        updateImageSize();
+      });
+    };
+  }
+
+  function updateImageSize() {
+    analyserRef.current.getByteFrequencyData(dataArrayRef.current);
+
+    let sum = 0;
+    for (let i = 0; i < dataArrayRef.current.length; i++) {
+      sum += dataArrayRef.current[i];
+    }
+    const average = sum / dataArrayRef.current.length;
+
+    setScale(1 + average / 800);
+    requestAnimationFrame(updateImageSize);
+  }
+
+  useEffect(() => {
+    setupAudioContext();
+    setupAudioSource();
+
+    startAudioVisualization();
+  }, []);
 
   return (
     <>
@@ -94,7 +121,6 @@ export const ThumbnailPreview = () => {
       >
         <div className="thumbnail-preview__background" style={{ backgroundImage: `url(${songs[currentSongIndex].cover})` }}></div>
         <figure className="thumbnail-preview__content">
-          {/* <div className="thumbnail-preview__image-warpper"> */}
           <div className="thumbnail-preview__image-warpper" style={{ transform: `scale(${scale})` }}>
             <img ref={coverArtRef} className="thumbnail-preview__image" src="img/not-found.jpg" alt="" />
             <div ref={transitionRef} className="thumbnail-preview__image-transition"></div>
