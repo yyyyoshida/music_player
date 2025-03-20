562498c46102aa38953a72477ad769506bcd0b90
yyyyoshida <thth331111@gmail.com>
2025-03-20 16:52:16 +0900

    音楽のリズムに合わせて曲の画像が動的に変化 (#11)

--- src/components/ThumbnailPreview.js
+++ src/components/ThumbnailPreview.js
@@ -1,7 +1,9 @@
 import React, { useContext, useRef, useState, useEffect } from 'react';
 import { TrackInfoContext } from './TrackInfoContext';
-import { songs } from './PlayMusic';
+import { songs, music } from './PlayMusic';
+// import { songs } from '../components/PlayMusic';
 import { usePlayerContext } from './PlayerContext';
+import { useAudioVisualizer } from '../hooks/useAudioVisualizer';
 
 export const ThumbnailPreview = () => {
   const { isVisible } = useContext(TrackInfoContext);
@@ -12,6 +14,8 @@ export const ThumbnailPreview = () => {
   const [artist, setArtist] = useState('');
   const coverArtRef = useRef(null);
   const transitionRef = useRef(null);
+  const scale = useAudioVisualizer();
+  // const [scale, setScale] = useState(1);
 
   function showThumbnail() {
     setDelayedVisibility('visible');
@@ -54,6 +58,33 @@ export const ThumbnailPreview = () => {
     fadeTransition();
   }, [currentSongIndex]);
 
+  // const audioContext = new (window.AudioContext || window.webkitAudioContext)();
+  // const analyser = audioContext.createAnalyser();
+  // const source = audioContext.createMediaElementSource(music);
+  // source.connect(analyser);
+  // analyser.connect(audioContext.destination);
+  // analyser.fftSize = 256; // 周波数の解像度
+  // const bufferLength = analyser.frequencyBinCount;
+  // const dataArray = new Uint8Array(bufferLength);
+
+  // function updateImageSize() {
+  //   analyser.getByteFrequencyData(dataArray);
+
+  //   let sum = 0;
+  //   for (let i = 0; i < bufferLength; i++) {
+  //     sum += dataArray[i];
+  //   }
+  //   const average = sum / bufferLength;
+  //   // const scale = 1 + average / 800;
+  //   setScale(1 + average / 800);
+  //   requestAnimationFrame(updateImageSize);
+  // }
+  // music.onplay = () => {
+  //   audioContext.resume().then(() => {
+  //     updateImageSize();
+  //   });
+  // };
+
   return (
     <>
       <div
@@ -63,7 +94,8 @@ export const ThumbnailPreview = () => {
       >
         <div className="thumbnail-preview__background" style={{ backgroundImage: `url(${songs[currentSongIndex].cover})` }}></div>
         <figure className="thumbnail-preview__content">
-          <div className="thumbnail-preview__image-warpper">
+          {/* <div className="thumbnail-preview__image-warpper"> */}
+          <div className="thumbnail-preview__image-warpper" style={{ transform: `scale(${scale})` }}>
             <img ref={coverArtRef} className="thumbnail-preview__image" src="img/not-found.jpg" alt="" />
             <div ref={transitionRef} className="thumbnail-preview__image-transition"></div>
           </div>
