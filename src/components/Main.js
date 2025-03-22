145fe63fe030a95bc4a3246827622c68da50df72
yyyyoshida <thth331111@gmail.com>
2025-03-22 17:29:49 +0900

    mainでの現在の作業を保存

--- src/components/Main.js
+++ src/components/Main.js
@@ -8,36 +8,39 @@ import { TrackInfoProvider } from './TrackInfoContext';
 const Main = () => {
   return (
     <div className="container">
-      <main>
+      <maidn>
         <PlayerProvider>
           <TrackInfoProvider>
             <ThumbnailPreview />
-            <section className="player">
+            <section className="search-result">
+              <h2 className="search-result__title">上位の検索結果</h2>
+              <ul className="search-result__list">
+                <li></li>
+              </ul>
+            </section>
+            {/* <section className="player">
               <div className="video-info">
                 <h2 id="js-video-title">動画タイトル</h2>
                 <p id="js-video-description">動画の説明がここに入ります。</p>
               </div>
 
-              {/* <!-- 動画（仮） --> */}
-              <div className="video-container">
-                {/* <!-- <iframe id="js-video-player" src="https://www.youtube.com/embed/example" frameborder="0" allowfullscreen></iframe> --> */}
-              </div>
 
-              {/* <!-- プレイヤーコントロール --> */}
-              <PlayerControls />
+
+
+         
               <div className="video-container">
                 <div id="js-video-player"></div>
               </div>
-              {/* <!-- <img id="bg-img" src="path/to/background.jpg" alt="Background Image"> -->
-					<!-- <img id="js-cover" src="path/to/default-cover.jpg" alt="Cover Image"> --> */}
+
               <h1 id="js-music-title">曲名がここに表示されます</h1>
               <div id="js-player-progress">0:00</div>
               <button id="js-prev-button">前の曲</button>
               <button id="js-next-button">次の曲</button>
-            </section>
+            </section> */}
+            <PlayerControls />
           </TrackInfoProvider>
         </PlayerProvider>
-      </main>
+      </maidn>
       <Footer />
     </div>
   );
