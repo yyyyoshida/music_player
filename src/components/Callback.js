59e9e7345fc5571b867340bfc1cb03e890ac2d46
yyyyoshida <thth331111@gmail.com>
2025-03-25 09:46:39 +0900

    難しすぎるむり

---
+++ src/components/Callback.js
@@ -0,0 +1,19 @@
+import React, { useEffect } from 'react';
+
+const Callback = () => {
+  useEffect(() => {
+    const hash = window.location.hash;
+    const token = new URLSearchParams(hash.replace('#', '?')).get('access_token');
+
+    if (token) {
+      localStorage.setItem('access_token', token); // アクセストークンをlocalStorageに保存
+      window.location.href = '/'; // メインページに遷移
+    } else {
+      console.error('アクセストークンの取得に失敗しました');
+    }
+  }, []);
+
+  return <div>ログイン中...</div>;
+};
+
+export default Callback;
