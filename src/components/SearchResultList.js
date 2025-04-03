40521a93f20f824aeb256420587237623d98d038
yyyyoshida <thth331111@gmail.com>
2025-04-03 18:11:54 +0900

    feat: 再生中のトラックにequalizerアニメーションと色変更機能を追加a

--- src/components/SearchResultList.js
+++ src/components/SearchResultList.js
@@ -4,27 +4,44 @@ import { SearchContext } from './SearchContext';
 
 const TrackList = () => {
   const { searchResults } = useContext(SearchContext);
-  const { playerTrack, formatTime } = usePlayerContext();
+  const { playerTrack, formatTime, isStreaming, trackId } = usePlayerContext();
 
   return (
     <ul className="search-result__list">
       {searchResults.length > 0 ? (
-        searchResults.map((track) => (
-          <li className="search-result__item" key={track.id} onClick={() => playerTrack(track.uri)}>
-            <div className="search-result__left"></div>
-            <img className="search-result__cover-art" src={track.album.images[0]?.url} alt={track.name} width="50" />
-            <div className="search-result__track-info">
-              <p className="search-result__name">{track.name}</p>
-              <p className="search-result__artist">{track.artists[0]?.name}</p>
-            </div>
-            <div className="search-result__right">
-              <button className="search-result__button--add">
-                <img className="search-result__icon--add" src="img/plus.png"></img>
-              </button>
-              <div className="search-result__track-duration">{formatTime(track.duration_ms)}</div>
-            </div>
-          </li>
-        ))
+        searchResults.map((track) => {
+          const isCurrentTrack = trackId === track.id;
+          const isTrackPlaying = isCurrentTrack && isStreaming;
+          const isClicked = isCurrentTrack;
+
+          return (
+            <li
+              key={track.id}
+              className={`search-result__item ${isTrackPlaying ? 'playing' : ''} ${isClicked ? 'clicked' : ''}`}
+              onClick={() => playerTrack(track.uri)}
+            >
+              <div className="search-result__left">
+                <div className={`equalizer ${isTrackPlaying ? '' : 'hidden'}`}>
+                  <div className="bar"></div>
+                  <div className="bar"></div>
+                  <div className="bar"></div>
+                  <div className="bar"></div>
+                </div>
+              </div>
+              <img className="search-result__cover-art" src={track.album.images[0]?.url} alt={track.name} width="50" />
+              <div className="search-result__track-info">
+                <p className="search-result__name">{track.name}</p>
+                <p className="search-result__artist">{track.artists[0]?.name}</p>
+              </div>
+              <div className="search-result__right">
+                <button className="search-result__button--add">
+                  <img className="search-result__icon--add" src="img/plus.png" />
+                </button>
+                <div className="search-result__track-duration">{formatTime(track.duration_ms)}</div>
+              </div>
+            </li>
+          );
+        })
       ) : (
         <li>検索結果がありません</li>
       )}
