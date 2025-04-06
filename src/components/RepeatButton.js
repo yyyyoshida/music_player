36fadd6b0b4cb26942204c6b9a5e56d59835925e
yyyyoshida <thth331111@gmail.com>
2025-04-06 20:39:41 +0900

    perf: repeatアイコンの読み込み遅延をbase64エンコードで改善

--- src/components/RepeatButton.js
+++ src/components/RepeatButton.js
@@ -1,13 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Tooltip from './Tooltip';
 import useButtonTooltip from '../hooks/useButtonTooltip';
 import { useRepeatContext } from './RepeatContext';
 import useDelayedText from '../hooks/useDelayText';
+import { repeatOnIcon, repeatOffIcon } from '../assets/icons';
 
 const RepeatButton = () => {
   const { isButtonPressed, isHovered, handleButtonPress, setIsHovered } = useButtonTooltip();
   const { isRepeat, toggleRepeat } = useRepeatContext();
   const tooltipText = useDelayedText('オン', 'オフ', isRepeat, isRepeat);
+
+  useEffect(() => {
+    console.log('isRepeatが変わった瞬間', isRepeat);
+  }, [isRepeat]);
 
   return (
     <button
@@ -19,7 +24,9 @@ const RepeatButton = () => {
       onMouseEnter={() => setIsHovered(true)}
       onMouseLeave={() => setIsHovered(false)}
     >
-      <img id="js-repeat-icon" src={isRepeat ? 'img/リピートオン.png' : 'img/リピート.png'} alt="Repeat Icon" />
+      {/* <img src={isRepeat ? 'img/repeat-on.png' : 'img/repeat.png'} alt="Repeat Icon" width="18" height="18" /> */}
+      {/* <img src={isRepeat ? 'img/repeat-on.png' : 'img/repeat.png'} alt="Repeat Icon" width="18" height="18" /> */}
+      <img src={isRepeat ? repeatOnIcon : repeatOffIcon} alt="Repeat Icon" width="18" height="18" />
 
       <Tooltip isHovered={isHovered} isButtonPressed={isButtonPressed} className={'tooltip-repeat'}>
         {`リピート：${tooltipText}`}
