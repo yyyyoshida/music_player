fd312d9160a3d5fb1fcd091009593280e159443f
yyyyoshida <thth331111@gmail.com>
2025-04-17 22:01:33 +0900

    fix: ページ遷移時やロード時のログインモーダルがちらつくのを改善

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { SearchProvider } from './components/SearchContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   // <React.StrictMode>
   // </React.StrictMode>
-  <App />
+  <SearchProvider>
+    <App />
+  </SearchProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
