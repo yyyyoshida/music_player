c04dc06725ecf58cc186597a88456124ddae3653
yyyyoshida <thth331111@gmail.com>
2025-04-24 09:54:47 +0900

    refactor: isToken専用のTokenContextを作成して各ファイルに適用

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
-import React, { useContext } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { SearchProvider } from './components/SearchContext';
+import { TokenProvider } from './contexts/isTokenContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   // <React.StrictMode>
   // </React.StrictMode>
-  <SearchProvider>
+  <TokenProvider>
     <App />
-  </SearchProvider>
+  </TokenProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
