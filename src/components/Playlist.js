7c579b938b6028475d9748bb8b690bc58eb9a1c0
yyyyoshida <thth331111@gmail.com>
2025-05-13 20:43:13 +0900

    feat: プレイリスト画像が3枚以下のときは1枚表示に機能を変更

--- src/components/Playlist.js
+++ src/components/Playlist.js
@@ -34,35 +34,32 @@ const Playlist = () => {
         </div>
       ) : (
         <ul className="playlists-page__list">
-          {playlists.map((playlist) => (
-            <li
-              key={playlist.id}
-              className="playlists-page__item"
-              onClick={() => {
-                handlePlaylistClick(playlist.id);
-              }}
-            >
-              <div className="playlists-page__item-cover-img-wrapper">
-                {[...playlist.albumImages].slice().map((src, i) => (
-                  <img key={i} src={src} className={`playlists-page__item-cover-img img-${i}`} />
-                ))}
+          {playlists.map((playlist) => {
+            const isSingleImage = playlist.albumImages.length <= 3;
+            const albumImagesToDisplay = [...playlist.albumImages].slice(0, 4);
+            console.log("isSingleImage", isSingleImage, playlist.name, albumImagesToDisplay.length);
+            return (
+              <li key={playlist.id} className="playlists-page__item" onClick={() => handlePlaylistClick(playlist.id)}>
+                <div className={`playlists-page__item-cover-img-wrapper ${isSingleImage ? "single" : ""}`}>
+                  {albumImagesToDisplay.map((src, i) => src && <img key={i} src={src} className={`playlists-page__item-cover-img img-${i}`} />)}
 
-                <img
-                  src="img/playlist-icon1.png"
-                  className="playlists-page__item-initial-cover-img playlist-initial-cover-img"
-                  style={{ visibility: playlist.trackCount === 0 ? "visible" : "hidden" }}
-                />
+                  <img
+                    src="img/playlist-icon1.png"
+                    className="playlists-page__item-initial-cover-img playlist-initial-cover-img"
+                    style={{ visibility: playlist.trackCount === 0 ? "visible" : "hidden" }}
+                  />
 
-                <button className="playlists-page__item-play-pause-button play-pause-button" style={{ visibility: "visible" }}>
-                  <img src={playIcon} className={`playlists-page__item-play-pause-button-icon play-pause-button-icon play-button-icon`} />
-                </button>
-              </div>
-              <div className="playlists-page__item-info">
-                <p className="playlists-page__item-name">{playlist.name}</p>
-                <span className="playlists-page__item-meta">{`${playlist.trackCount}曲, ${formatTimeHours(playlist.totalDuration)}`}</span>
-              </div>
-            </li>
-          ))}
+                  <button className="playlists-page__item-play-pause-button play-pause-button">
+                    <img src={playIcon} className="playlists-page__item-play-pause-button-icon play-pause-button-icon play-button-icon" />
+                  </button>
+                </div>
+                <div className="playlists-page__item-info">
+                  <p className="playlists-page__item-name">{playlist.name}</p>
+                  <span className="playlists-page__item-meta">{`${playlist.trackCount}曲, ${formatTimeHours(playlist.totalDuration)}`}</span>
+                </div>
+              </li>
+            );
+          })}
         </ul>
       )}
     </div>
