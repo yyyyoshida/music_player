938dd4face5da5e9eddeb9a753072f97fe32f3b4
yyyyoshida <thth331111@gmail.com>
2025-05-13 10:22:45 +0900

    feat: プレイリスト一覧を古い順に並び替え (#13)

--- src/hooks/useFetchPlaylists.js
+++ src/hooks/useFetchPlaylists.js
@@ -1,5 +1,5 @@
 import { useState, useEffect, useContext } from "react";
-import { collection, onSnapshot, getDocs } from "firebase/firestore";
+import { collection, onSnapshot, getDocs, query, orderBy } from "firebase/firestore";
 import { db } from "../firebase";
 import { LoadingContext } from "../contexts/LoadingContext";
 
@@ -10,32 +10,34 @@ const useFetchPlaylists = () => {
 
   useEffect(() => {
     console.log("playlistsカスタムフック発火");
-    const playlistsRef = collection(db, "playlists");
 
-    const unsubscribe = onSnapshot(playlistsRef, async (snapshot) => {
-      // setLoading(true);
+    const playlistsQuery = query(collection(db, "playlists"), orderBy("createdAt", "asc"));
+
+    const unsubscribe = onSnapshot(playlistsQuery, async (snapshot) => {
       startLoading();
 
       const playlistsData = await Promise.all(
         snapshot.docs.map(async (doc) => {
           const playlistId = doc.id;
+
           const tracksSnapshot = await getDocs(collection(db, "playlists", playlistId, "tracks"));
+
           const tracks = tracksSnapshot.docs.map((doc) => doc.data());
+
           return {
             id: playlistId,
             ...doc.data(),
             trackCount: tracksSnapshot.size || 0,
             albumImages: tracks
-              .slice() // コピーして
-              .sort((a, b) => a.addedAt?.seconds - b.addedAt?.seconds) // 追加が古い順
-              .slice(0, 4) // 最初の4つ
+              .slice()
+              .sort((a, b) => a.addedAt?.seconds - b.addedAt?.seconds)
+              .slice(0, 4)
               .map((track) => track.albumImage),
           };
         })
       );
 
       setPlaylists(playlistsData);
-      // setLoading(false);
       stopLoading();
     });
 
