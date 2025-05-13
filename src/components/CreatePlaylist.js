2b326c5bfd9c65a0c3b832838ca1a7e5aa2981b1
yyyyoshida <thth331111@gmail.com>
2025-05-13 14:19:20 +0900

    feat: プレイリスト名変更のバリデーション・エラー表示・アニメーション対応 (#13)

--- src/components/CreatePlaylist.js
+++ src/components/CreatePlaylist.js
@@ -1,11 +1,12 @@
-import React, { useContext, useRef } from 'react';
-import { PlaylistContext } from './PlaylistContext';
+import React, { useContext, useRef } from "react";
+import { PlaylistContext } from "./PlaylistContext";
+import { warningIcon } from "../assets/icons";
 
 const CreatePlaylist = () => {
-  const { toggleCreateVisible, handleCreatePlaylist, isCreateVisible, playlistNameRef } = useContext(PlaylistContext);
+  const { isShaking, errorMessage, toggleCreateVisible, handleCreatePlaylist, isCreateVisible, playlistNameRef } = useContext(PlaylistContext);
 
   return (
-    <div className="playlist-page__create-playlist-modal modal" style={{ visibility: isCreateVisible ? 'visible' : 'hidden' }}>
+    <div className="playlist-page__create-playlist-modal modal" style={{ visibility: isCreateVisible ? "visible" : "hidden" }}>
       <div className="playlist-page__create-playlist-modal-smoke modal-smoke">
         <div className="playlist-page__create-playlist-modal-content modal-content">
           <h2 className="playlist-page__create-playlist-modal-title modal-title">新しいプレイリスト</h2>
@@ -16,19 +17,25 @@ const CreatePlaylist = () => {
             <label className="playlist-page__create-playlist-modal-label modal-label" htmlFor="title">
               タイトル
             </label>
-            <input className="playlist-page__create-playlist-modal-input modal-input" id="title" ref={playlistNameRef} />
+            <input
+              className="playlist-page__create-playlist-modal-input modal-input"
+              id="title"
+              ref={playlistNameRef}
+              onKeyDown={(e) => e.key === "Enter" && handleCreatePlaylist()}
+            />
+
+            {errorMessage && (
+              <p className={`error-text ${isShaking ? "shake" : ""}`}>
+                <img src={warningIcon} />
+                {errorMessage}
+              </p>
+            )}
           </div>
           <div className="playlist-page__create-playlist-modal-actions modal-actions">
-            <button
-              className="playlist-page__create-playlist-modal-cancel modal-cancel-submit-button modal-cancel-button"
-              onClick={toggleCreateVisible}
-            >
+            <button className="playlist-page__create-playlist-modal-cancel modal-cancel-submit-button modal-cancel-button" onClick={toggleCreateVisible}>
               キャンセル
             </button>
-            <button
-              className="playlist-page__create-playlist-modal-create modal-cancel-submit-button modal-submit-button"
-              onClick={handleCreatePlaylist}
-            >
+            <button className="playlist-page__create-playlist-modal-create modal-cancel-submit-button modal-submit-button" onClick={handleCreatePlaylist}>
               作成
             </button>
           </div>
