dce739cb98d753aec2a9d207867525ed978a4e4b
yyyyoshida <thth331111@gmail.com>
2025-05-16 20:51:09 +0900

    feat: プレイリスト曲一覧と最近再生の曲一覧にスケルトンUIを追加

--- src/components/Playlist.js
+++ src/components/Playlist.js
@@ -5,6 +5,7 @@ import useFetchPlaylists from "../hooks/useFetchPlaylists";
 import { LoadingContext } from "../contexts/LoadingContext";
 import { PlaylistSelectionContext } from "./PlaylistSelectionContext";
 import { playIcon, pauseIcon } from "../assets/icons";
+import CardListSkeleton from "./CardListSkeleton";
 
 const Playlist = () => {
   const { toggleCreateVisible, formatTimeHours } = useContext(PlaylistContext);
@@ -20,48 +21,39 @@ const Playlist = () => {
   return (
     <div className="playlists-page">
       <h2 className="playlists-page__title">プレイリスト</h2>
-
       <button className="playlists-page__create-button playlist-create-button" onClick={toggleCreateVisible}>
         ＋ 新規プレイリスト作成
       </button>
+      {isLoading && !isSelectVisible && <CardListSkeleton />}
 
-      {isLoading && !isSelectVisible ? (
-        <div className="loading">
-          <div className="loading__content">
-            <p className="loading__text">読み込み中</p>
-            <div className="loading__spinner loader"></div>
-          </div>
-        </div>
-      ) : (
-        <ul className="playlists-page__list">
-          {playlists.map((playlist) => {
-            const isSingleImage = playlist.albumImages.length <= 3;
-            const albumImagesToDisplay = [...playlist.albumImages].slice(0, 4);
-            console.log("isSingleImage", isSingleImage, playlist.name, albumImagesToDisplay.length);
-            return (
-              <li key={playlist.id} className="playlists-page__item" onClick={() => handlePlaylistClick(playlist.id)}>
-                <div className={`playlists-page__item-cover-img-wrapper ${isSingleImage ? "single" : ""}`}>
-                  {albumImagesToDisplay.map((src, i) => src && <img key={i} src={src} className={`playlists-page__item-cover-img img-${i}`} />)}
+      <ul className={`playlists-page__list fade-on-loaded ${isLoading ? "" : "fade-in"}`}>
+        {playlists.map((playlist) => {
+          const isSingleImage = playlist.albumImages.length <= 3;
+          const albumImagesToDisplay = [...playlist.albumImages].slice(0, 4);
+          console.log("isSingleImage", isSingleImage, playlist.name, albumImagesToDisplay.length);
+          return (
+            <li key={playlist.id} className="playlists-page__item" onClick={() => handlePlaylistClick(playlist.id)}>
+              <div className={`playlists-page__item-cover-img-wrapper ${isSingleImage ? "single" : ""}`}>
+                {albumImagesToDisplay.map((src, i) => src && <img key={i} src={src} className={`playlists-page__item-cover-img img-${i}`} />)}
 
-                  <img
-                    src="img/playlist-icon1.png"
-                    className="playlists-page__item-initial-cover-img playlist-initial-cover-img"
-                    style={{ visibility: playlist.trackCount === 0 ? "visible" : "hidden" }}
-                  />
+                <img
+                  src="img/playlist-icon1.png"
+                  className="playlists-page__item-initial-cover-img playlist-initial-cover-img"
+                  style={{ visibility: playlist.trackCount === 0 ? "visible" : "hidden" }}
+                />
 
-                  <button className="playlists-page__item-play-pause-button play-pause-button">
-                    <img src={playIcon} className="playlists-page__item-play-pause-button-icon play-pause-button-icon play-button-icon" />
-                  </button>
-                </div>
-                <div className="playlists-page__item-info">
-                  <p className="playlists-page__item-name">{playlist.name}</p>
-                  <span className="playlists-page__item-meta">{`${playlist.trackCount}曲, ${formatTimeHours(playlist.totalDuration)}`}</span>
-                </div>
-              </li>
-            );
-          })}
-        </ul>
-      )}
+                <button className="playlists-page__item-play-pause-button play-pause-button">
+                  <img src={playIcon} className="playlists-page__item-play-pause-button-icon play-pause-button-icon play-button-icon" />
+                </button>
+              </div>
+              <div className="playlists-page__item-info">
+                <p className="playlists-page__item-name">{playlist.name}</p>
+                <span className="playlists-page__item-meta">{`${playlist.trackCount}曲, ${formatTimeHours(playlist.totalDuration)}`}</span>
+              </div>
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 };
