634524c92a3787e06914eb8a1c9b542e0ecdd9eb
yyyyoshida <thth331111@gmail.com>
2025-05-22 21:54:28 +0900

    feat: 検索結果のテキスト&UIを一部変更

--- src/components/SearchResult.js
+++ src/components/SearchResult.js
@@ -4,12 +4,14 @@ import { SearchContext } from "../contexts/SearchContext";
 import TrackListHead from "./TrackListHead";
 
 const SearchResult = ({ containerRef }) => {
-  const { query } = useContext(SearchContext);
+  const { query, hasSearchError } = useContext(SearchContext);
   return (
     <section className="search-result">
       <div className="search-result__header">
-        <h2 className="search-result__title">{`${query}の検索結果`}</h2>
-
+        <h2 className="search-result__title">
+          <span className="search-result-highlight">{query}</span>
+          {!hasSearchError ? "の検索結果" : "の検索結果が見つかりませんでした。"}
+        </h2>
         <TrackListHead />
       </div>
 
