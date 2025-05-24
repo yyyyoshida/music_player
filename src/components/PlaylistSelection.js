a80328ed8f73d2e998a7fb438f9c938a28572b3c
yyyyoshida <thth331111@gmail.com>
2025-05-24 16:50:17 +0900

    perf: プレイリスト選択画面にローディング待機処理とスケルトンUIを追加

--- src/components/PlaylistSelection.js
+++ src/components/PlaylistSelection.js
@@ -2,37 +2,17 @@ import { useContext, useRef, useEffect } from "react";
 import { PlaylistSelectionContext } from "../contexts/PlaylistSelectionContext";
 import { PlaylistContext } from "../contexts/PlaylistContext";
 import useFetchPlaylists from "../hooks/useFetchPlaylists";
-import { LoadingContext } from "../contexts/LoadingContext";
+
+import PlaylistSelectSkeleton from "./PlaylistSelectSkeleton";
+import useWaitForImagesLoad from "../hooks/useWaitForImagesLoad";
 
 const PlaylistSelection = () => {
   const { isSelectVisible, toggleSelectVisible, addTrackToPlaylist } = useContext(PlaylistSelectionContext);
   const { toggleCreateVisible } = useContext(PlaylistContext);
   const { playlists } = useFetchPlaylists();
-  const { isLoading } = useContext(LoadingContext);
+  const imagesLoaded = useWaitForImagesLoad("playlistCover", playlists, [playlists], 0);
 
   const coverImagesRefs = useRef([]);
-
-  const FADE_DURATION = 300;
-
-  useEffect(() => {
-    const timeoutIds = [];
-
-    coverImagesRefs.current.forEach((element) => {
-      if (element) {
-        element.style.opacity = "0";
-
-        const timeoutId = setTimeout(() => {
-          element.style.opacity = "1";
-        }, FADE_DURATION);
-
-        timeoutIds.push(timeoutId);
-      }
-    });
-
-    return () => {
-      timeoutIds.forEach((id) => clearTimeout(id));
-    };
-  }, [playlists]);
 
   return (
     <div className="playlist-selection modal" style={{ visibility: isSelectVisible ? "visible" : "hidden" }}>
@@ -52,46 +32,37 @@ const PlaylistSelection = () => {
             ＋ 新しいプレイリスト作成
           </button>
 
-          {isLoading && !isSelectVisible ? (
-            // {/* {isLoading ? ( */}
-            <div className="loading mini-loading">
-              <div className="loading__content mini-loading-content">
-                <p className="loading__text mini-loading-text">読み込み中</p>
-                <div className="loading__spinner mini-loading-loading-spinner loader"></div>
-              </div>
-            </div>
-          ) : (
-            <ul className="playlist-selection__list">
-              {playlists.map((playlist, i) => {
-                const isSingleImage = playlist.albumImages.length <= 3;
-                return (
-                  <li
-                    key={playlist.id}
-                    className="playlist-selection__item"
-                    onClick={() => {
-                      addTrackToPlaylist(playlist.id);
-                    }}
-                  >
-                    <div className="playlist-selection__item-cover-img-wrapper">
-                      <div className={`playlist-selection__item-cover-imgs ${isSingleImage && "single"}`} ref={(el) => (coverImagesRefs.current[i] = el)}>
-                        {playlist.albumImages.map((src, i) => (
-                          <img key={i} src={src} className={`playlist-selection__item-cover-img img-${i}`} />
-                        ))}
-                      </div>
-                      <div className="playlist-selection__item-initial-cover-img-bg">
-                        <img
-                          src="/img/playlist-icon1.png"
-                          className="playlist-selection__item-initial-cover-img playlist-initial-cover-img"
-                          // style={{ display: playlist.albumImages.length === 0 ? "block" : "none" }}
-                        />
-                      </div>
+          {!imagesLoaded && <PlaylistSelectSkeleton />}
+          <ul className={`playlist-selection__list ${imagesLoaded ? "fade-in-up" : ""}`}>
+            {playlists.map((playlist, i) => {
+              const isSingleImage = playlist.albumImages.length <= 3;
+              return (
+                <li
+                  key={playlist.id}
+                  className="playlist-selection__item"
+                  onClick={() => {
+                    addTrackToPlaylist(playlist.id);
+                  }}
+                >
+                  <div className="playlist-selection__item-cover-img-wrapper">
+                    <div className={`playlist-selection__item-cover-imgs ${isSingleImage && "single"}`} ref={(el) => (coverImagesRefs.current[i] = el)}>
+                      {playlist.albumImages.map((src, i) => (
+                        <img key={i} src={src} className={`playlist-selection__item-cover-img img-${i}`} />
+                      ))}
                     </div>
-                    <p className="playlist-selection__item-name">{playlist.name}</p>
-                  </li>
-                );
-              })}
-            </ul>
-          )}
+                    <div className="playlist-selection__item-initial-cover-img-bg">
+                      <img
+                        src="/img/playlist-icon1.png"
+                        className="playlist-selection__item-initial-cover-img playlist-initial-cover-img"
+                        // style={{ display: playlist.albumImages.length === 0 ? "block" : "none" }}
+                      />
+                    </div>
+                  </div>
+                  <p className="playlist-selection__item-name">{playlist.name}</p>
+                </li>
+              );
+            })}
+          </ul>
         </div>
       </div>
     </div>
