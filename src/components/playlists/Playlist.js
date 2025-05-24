10b45a2997719b0a0c52742cad0630183897d704
yyyyoshida <thth331111@gmail.com>
2025-05-24 18:13:07 +0900

    refactor: Skeletonsフォルダーを小文字に統一しつつ名前を変更して一貫性を持たせた

--- src/components/playlists/Playlist.js
+++ src/components/playlists/Playlist.js
@@ -4,7 +4,7 @@ import { PlaylistContext } from "../../contexts/PlaylistContext";
 import useFetchPlaylists from "../../hooks/useFetchPlaylists";
 
 import { playIcon, pauseIcon } from "../../assets/icons";
-import CardListSkeleton from "../Skeletons/CardListSkeleton";
+import CardListSkeleton from "../skeletonUI/CardListSkeleton";
 import useWaitForImagesLoad from "../../hooks/useWaitForImagesLoad";
 
 const Playlist = () => {
