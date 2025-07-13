e0666d4e8cd8a4a36491700434f65b70f72af3a7
yyyyoshida <thth331111@gmail.com>
2025-07-13 09:26:04 +0900

    refactor: plus画像とfavorite画像のパスを定数化

--- src/components/tracks/TrackMoreMenu.js
+++ src/components/tracks/TrackMoreMenu.js
@@ -1,7 +1,7 @@
 import { useContext, useRef, useEffect } from "react";
+import { FAVORITE_ICON, ADD_TO_PLAYLIST_ICON } from "../../assets/icons";
 import { TrackMoreMenuContext } from "../../contexts/TrackMoreMenuContext";
 import { PlaylistSelectionContext } from "../../contexts/PlaylistSelectionContext";
-
 import { PlaylistContext } from "../../contexts/PlaylistContext";
 
 const TrackMoreMenu = () => {
@@ -38,7 +38,7 @@ const TrackMoreMenu = () => {
     <div className={`track-more-menu ${isMenuVisible && "is-open-menu"}`} style={{ top: menuPositionTop }} ref={menuRef}>
       <ul className="track-more-menu__list">
         <li className="track-more-menu__item">
-          <img src="/img/heart.png" className="track-more-menu__item-icon-favorite" />
+          <img src={FAVORITE_ICON} className="track-more-menu__item-icon-favorite" />
           お気に入りに追加
         </li>
         <li
@@ -48,7 +48,7 @@ const TrackMoreMenu = () => {
             setIsMenuVisible(false);
           }}
         >
-          <img src="/img/plus.png" className="track-more-menu__item-icon-add" />
+          <img src={ADD_TO_PLAYLIST_ICON} className="track-more-menu__item-icon-add" />
           プレイリストに追加
         </li>
         {isNotSearchPage && (
