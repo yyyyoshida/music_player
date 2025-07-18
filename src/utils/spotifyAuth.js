cddae7d167f64bbef7705b81433c19b96749de4a
yyyyoshida <thth331111@gmail.com>
2025-07-18 11:53:46 +0900

    refactor: SpotifyとFirebaseの機密情報を.envに移動

--- src/utils/spotifyAuth.js
+++ src/utils/spotifyAuth.js
@@ -1,18 +1,17 @@
-const CLIENT_ID = 'a97251d0ab5e40c7bc41a3997e166e2d'; // あなたのクライアントID
-const CLIENT_SECRET = '56318a7b9973426cbbfa2abcbf864ca0'; // あなたのクライアントシークレット
-const REFRESH_TOKEN = window.localStorage.getItem('refresh_token'); // ローカルストレージからリフレッシュトークンを取得
+const CLIENT_ID = process.env.REACT_APP_SPOTIFY_API_CLIENT_ID;
+const CLIENT_SECRET = process.env.REACT_APP_SPOTIFY_API_CLIENT_SECRET;
 
 const getNewAccessToken = async () => {
-  const refreshToken = window.localStorage.getItem('refresh_token');
+  const refreshToken = window.localStorage.getItem("refresh_token");
 
-  const response = await fetch('https://accounts.spotify.com/api/token', {
-    method: 'POST',
+  const response = await fetch("https://accounts.spotify.com/api/token", {
+    method: "POST",
     headers: {
-      'Content-Type': 'application/x-www-form-urlencoded',
-      Authorization: 'Basic ' + btoa(CLIENT_ID + ':' + CLIENT_SECRET),
+      "Content-Type": "application/x-www-form-urlencoded",
+      Authorization: "Basic " + btoa(CLIENT_ID + ":" + CLIENT_SECRET),
     },
     body: new URLSearchParams({
-      grant_type: 'refresh_token',
+      grant_type: "refresh_token",
       refresh_token: refreshToken,
     }),
   });
@@ -20,9 +19,9 @@ const getNewAccessToken = async () => {
   const data = await response.json();
 
   if (data.access_token) {
-    window.localStorage.setItem('access_token', data.access_token); // 新しいトークンを保存
+    window.localStorage.setItem("access_token", data.access_token); // 新しいトークンを保存
     return data.access_token;
   }
-  throw new Error('アクセストークンの更新に失敗しました');
+  throw new Error("アクセストークンの更新に失敗しました");
 };
-export { getNewAccessToken }; // 他のファイルで利用できるようにエクスポート
+export { getNewAccessToken };
