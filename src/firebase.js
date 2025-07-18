e3aeac43c001fd7dc1d051b0b57053cac0c1fac9
yyyyoshida <thth331111@gmail.com>
2025-07-18 11:53:46 +0900

    refactor: SpotifyとFirebaseの機密情報を.envに移動

--- src/firebase.js
+++ src/firebase.js
@@ -1,21 +1,16 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAfwrJJbw54FeKYKyN4-GmmoZ7Fn1ciAaE",
-  authDomain: "my-music-player-8ae45.firebaseapp.com",
-  projectId: "my-music-player-8ae45",
-  storageBucket: "my-music-player-8ae45.firebasestorage.app",
-  messagingSenderId: "651199236297",
-  appId: "1:651199236297:web:5ba17f2cadcdf7d654a518",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
