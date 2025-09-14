d13f8f0c8c2d282176724918c3fae706248e02ed
yyyyoshida <thth331111@gmail.com>
2025-09-14 20:58:37 +0900

    refactor: 曲の再生状態同期ロジックをZustandに移行 #74

--- src/contexts/PlayerContext.js
+++ src/contexts/PlayerContext.js
@@ -1,5 +1,5 @@
 import { createContext, useState, useContext, useEffect, useRef } from "react";
-import { useRepeatContext } from "./RepeatContext";
+// import { useRepeatContext } from "./RepeatContext";
 import usePlayerStore from "../store/playerStore";
 
 const PlayerContext = createContext();
@@ -8,50 +8,9 @@ export const PlayerProvider = ({ children, isTrackSet, setIsTrackSet }) => {
   const setCurrentTime = usePlayerStore((state) => state.setCurrentTime);
   const setPosition = usePlayerStore((state) => state.setPosition);
   const setDuration = usePlayerStore((state) => state.setDuration);
-  const setTrackId = usePlayerStore((state) => state.setTrackId);
-  const isSpotifyPlaying = usePlayerStore((state) => state.isSpotifyPlaying);
+
   const isLocalPlaying = usePlayerStore((state) => state.isLocalPlaying);
   const audioRef = usePlayerStore((state) => state.audioRef);
-  const player = usePlayerStore((state) => state.player);
-
-  const { isRepeat } = useRepeatContext();
-
-  useEffect(() => {
-    if (!player || !isSpotifyPlaying) return;
-
-    player.addListener("player_state_changed", (state) => {
-      if (!state || !state.track_window?.current_track) return;
-
-      const {
-        position,
-        duration,
-        track_window: { current_track },
-        paused,
-      } = state;
-
-      setPosition((position / duration) * 100);
-      setDuration(duration);
-      setTrackId(current_track.id);
-    });
-
-    const interval = setInterval(() => {
-      player.getCurrentState().then((state) => {
-        if (!state) return;
-
-        const { position, duration } = state;
-
-        if (typeof position === "number" && typeof duration === "number") {
-          setPosition((position / duration) * 100);
-          setCurrentTime(position);
-        }
-      });
-    }, 200);
-
-    return () => {
-      clearInterval(interval);
-      player.removeListener("player_state_changed");
-    };
-  }, [player, isRepeat, isSpotifyPlaying]);
 
   useEffect(() => {
     if (!audioRef?.current || !isLocalPlaying) return;
