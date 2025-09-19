123f52232f4369d76802f499cadf26ee237c9f57
yyyyoshida <thth331111@gmail.com>
2025-09-19 15:23:19 +0900

    refactor: App.js内のマウント時のトークンの初期化をカスタムフックに切り出し

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-import { useState, useEffect, useContext } from "react";
-
+import { useState, useEffect } from "react";
 import { BrowserRouter } from "react-router-dom";
-import { getNewAccessToken, saveRefreshToken, getRefreshToken, isValidToken } from "./utils/spotifyAuth";
 
 import { SearchProvider } from "./contexts/SearchContext";
-import useTokenStore from "./store/tokenStore";
 import usePlayerStore from "./store/playerStore";
+import useInitSpotifyToken from "./hooks/useInitSpotifyToken";
 
 import Header from "./components/Header";
 import Main from "./components/Main";
@@ -14,8 +12,6 @@ function App() {
   const [profile, setProfile] = useState(null);
   const [searchResults, setSearchResults] = useState([]);
 
-  const setToken = useTokenStore((state) => state.setToken);
-  const setIsToken = useTokenStore((state) => state.setIsToken);
   const initPlayer = usePlayerStore((state) => state.initPlayer);
   const player = usePlayerStore((state) => state.player);
   const isSpotifyPlaying = usePlayerStore((state) => state.isSpotifyPlaying);
@@ -23,78 +19,11 @@ function App() {
   const syncSpotifyPlayerState = usePlayerStore((state) => state.syncSpotifyPlayerState);
   const syncLocalAudioState = usePlayerStore((state) => state.syncLocalAudioState);
 
-  useEffect(() => {
-    async function init() {
-      const code = new URLSearchParams(window.location.search).get("code");
-      const localAccessToken = localStorage.getItem("access_token");
-      const localRefreshToken = localStorage.getItem("refresh_token");
-
-      // ローカルのトークンでログイン
-      if (localAccessToken && (await isValidToken(localAccessToken))) {
-        setToken(localAccessToken);
-        return;
-      }
-
-      // ローカルのリフレッシュトークンでログイン
-      if (localRefreshToken) {
-        try {
-          const newToken = await getNewAccessToken();
-          setToken(newToken);
-          return;
-        } catch (error) {
-          console.warn("ローカルリフレッシュ失敗(次の手段でログイン):", error);
-        }
-      }
-
-      // 初回ログイン後にトークンとリフレッシュトークンを保存
-      if (code) {
-        try {
-          const res = await fetch("http://localhost:4000/api/exchange_token", {
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify({ code }),
-          });
-
-          const data = await res.json();
-
-          if (data.access_token) {
-            localStorage.setItem("access_token", data.access_token);
-            setToken(data.access_token);
-          }
-
-          if (data.refresh_token) {
-            localStorage.setItem("refresh_token", data.refresh_token);
-            await saveRefreshToken(data.refresh_token);
-          }
-
-          window.history.replaceState({}, null, "/");
-          return;
-        } catch (error) {
-          console.warn("トークン交換失敗(次の手段でログイン):", error);
-        }
-      }
-
-      // DBに保存されたリフレッシュトークンでログイン
-      if (!localAccessToken && !localRefreshToken) {
-        try {
-          const storedRefreshToken = await getRefreshToken();
-          const newToken = await getNewAccessToken(storedRefreshToken);
-          setToken(newToken);
-          localStorage.setItem("access_token", newToken);
-          localStorage.setItem("refresh_token", storedRefreshToken);
-
-          if (!storedRefreshToken) throw new Error("リフレッシュトークンがサーバーにない");
-        } catch (error) {
-          setIsToken(false);
-          console.error("🔁 トークンの更新失敗 ログインしてください:", error);
-        }
-      }
-    }
-    init();
-  }, []);
+  useInitSpotifyToken();
 
   useEffect(() => {
     let instance;
+
     (async () => {
       instance = await initPlayer();
     })();
---
+++ src/hooks/useInitSpotifyToken.js
@@ -0,0 +1,94 @@
+import { useEffect } from "react";
+import { isValidToken, getNewAccessToken, getRefreshToken, saveRefreshToken } from "../utils/spotifyAuth";
+import useTokenStore from "../store/tokenStore";
+
+const useInitSpotifyToken = () => {
+  const setToken = useTokenStore((state) => state.setToken);
+  const setIsToken = useTokenStore((state) => state.setIsToken);
+
+  async function initTokenFromCache() {
+    const localAccessToken = localStorage.getItem("access_token");
+    const localRefreshToken = localStorage.getItem("refresh_token");
+
+    // ローカルのトークンでログイン
+    if (localAccessToken && (await isValidToken(localAccessToken))) {
+      setToken(localAccessToken);
+      console.log("ローカルToken");
+      return true;
+    }
+
+    // ローカルのリフレッシュトークンでログイン
+    if (localRefreshToken) {
+      try {
+        const newToken = await getNewAccessToken();
+        setToken(newToken);
+        console.log("ローカルrefresh");
+        return true;
+      } catch (error) {
+        console.warn("ローカルリフレッシュ失敗(次の手段でログイン):", error);
+      }
+    }
+
+    return false;
+  }
+
+  async function initTokenFromCode() {
+    const code = new URLSearchParams(window.location.search).get("code");
+    if (!code) return false;
+
+    try {
+      const res = await fetch("http://localhost:4000/api/exchange_token", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ code }),
+      });
+
+      const data = await res.json();
+
+      if (data.access_token) {
+        localStorage.setItem("access_token", data.access_token);
+        setToken(data.access_token);
+      }
+
+      if (data.refresh_token) {
+        localStorage.setItem("refresh_token", data.refresh_token);
+        await saveRefreshToken(data.refresh_token);
+      }
+
+      window.history.replaceState({}, null, "/");
+      console.log("code");
+      return true;
+    } catch (error) {
+      console.warn("トークン交換失敗(次の手段でログイン):", error);
+      return false;
+    }
+  }
+
+  async function initTokenFromDB() {
+    try {
+      const storedRefreshToken = await getRefreshToken();
+      if (!storedRefreshToken) throw new Error("リフレッシュトークンがサーバーにない");
+
+      const newToken = await getNewAccessToken(storedRefreshToken);
+      setToken(newToken);
+      localStorage.setItem("access_token", newToken);
+      localStorage.setItem("refresh_token", storedRefreshToken);
+      console.log("DB");
+      return true;
+    } catch (error) {
+      setIsToken(false);
+      console.error("🔁 トークンの更新失敗 ログインしてください:", error);
+      return false;
+    }
+  }
+
+  useEffect(() => {
+    (async () => {
+      if (await initTokenFromCache()) return;
+      if (await initTokenFromCode()) return;
+      await initTokenFromDB();
+    })();
+  }, []);
+};
+
+export default useInitSpotifyToken;
