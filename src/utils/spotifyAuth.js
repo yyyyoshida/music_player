85ca990d6c37b6408c245cdf3da96a08f2859909
yyyyoshida <thth331111@gmail.com>
2025-09-23 19:29:08 +0900

    fix: 毎ロード時にトークンが更新されてしまうバグを修正

--- src/utils/spotifyAuth.js
+++ src/utils/spotifyAuth.js
@@ -105,6 +105,7 @@ function loadSpotifySDK() {
 
 export async function initSpotifyPlayer(setPlayer, setDeviceId, setToken) {
   const DEFAULT_VOLUME = 0.3;
+  const currentToken = window.localStorage.getItem("access_token");
 
   await loadSpotifySDK();
 
@@ -112,10 +113,17 @@ export async function initSpotifyPlayer(setPlayer, setDeviceId, setToken) {
     const playerInstance = new window.Spotify.Player({
       name: "MyMusicPlayer",
       getOAuthToken: async (cb) => {
-        const newToken = await getNewAccessToken();
-        setToken(newToken);
+        if (!isValidToken(currentToken)) {
+          const newToken = await getNewAccessToken();
+          setToken(newToken);
 
-        cb(newToken);
+          cb(newToken);
+          return;
+        }
+
+        setToken(currentToken);
+
+        cb(currentToken);
       },
       volume: DEFAULT_VOLUME,
     });
