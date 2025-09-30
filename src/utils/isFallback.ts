b844793d03ccdddbfc2cb7b0aa31f673f24d86c3
yyyyoshida <thth331111@gmail.com>
2025-09-30 21:28:35 +0900

    refactor: isFallbackファイルをts化 #107

--- src/utils/isFallback.ts
+++ src/utils/isFallback.ts
@@ -1,5 +1,6 @@
 import { FALLBACK_COVER_IMAGE } from "../assets/icons";
-export function isFallback(imgSrc) {
+
+export function isFallback(imgSrc: string): boolean {
   if (typeof imgSrc !== "string") return false;
 
   return imgSrc.endsWith(FALLBACK_COVER_IMAGE);
