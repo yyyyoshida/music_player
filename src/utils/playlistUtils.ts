bf1dcadd576b36a4ec4abe8ca1778acb225f5879
yyyyoshida <thth331111@gmail.com>
2025-10-04 06:47:57 +0900

    refactor: playlistStoreファイルをts化 #109

--- src/utils/playlistUtils.ts
+++ src/utils/playlistUtils.ts
@@ -2,29 +2,30 @@ export async function getPlaylistInfo(
   currentPlaylistId: string,
   setPlaylistInfo: (info: { name: string; totalDuration: number }) => void,
   showMessage: (key: string) => void
-): Promise<void> {
+): Promise<{ name: string; totalDuration: number }> {
   const cachedPlaylistInfo = localStorage.getItem(`playlistDetail:${currentPlaylistId}Info`);
   const BASE_URL = process.env.REACT_APP_API_BASE_URL;
 
   if (cachedPlaylistInfo) {
     setPlaylistInfo(JSON.parse(cachedPlaylistInfo));
-    return;
+    return JSON.parse(cachedPlaylistInfo);
   }
 
   try {
     const response = await fetch(`${BASE_URL}/api/playlists/${currentPlaylistId}/info`);
 
     if (!response.ok) {
-      getPlaylistInfoFailed(response.status);
-      return;
+      throw new Error(String(response.status));
     }
 
     const data = await response.json();
     localStorage.setItem(`playlistDetail:${currentPlaylistId}Info`, JSON.stringify(data));
 
     setPlaylistInfo(data);
+    return data;
   } catch (error) {
     getPlaylistInfoFailed(error);
+    return { name: "プレイリスト", totalDuration: 0 };
   }
 
   function getPlaylistInfoFailed(logValue: any) {
