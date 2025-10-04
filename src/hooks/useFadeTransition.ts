666d5c944a56d22774c4f93ea4658f8b3b44bc96
yyyyoshida <thth331111@gmail.com>
2025-10-04 20:14:04 +0900

    refactor: useFadeTransitionファイルをts化 #111

--- src/hooks/useFadeTransition.ts
+++ src/hooks/useFadeTransition.ts
@@ -1,17 +1,17 @@
 import { useEffect } from "react";
 
-const useFadeTransition = (transitionRef, trigger) => {
+const useFadeTransition = (transitionRef: React.RefObject<HTMLElement>, trigger: string) => {
   const TRANSITION_DELAY_MS = 50;
   function fadeTransition() {
     const element = transitionRef.current;
     element.style.visibility = "visible";
-    element.style.opacity = 1;
+    element.style.opacity = "1";
     function handleTransitionEnd() {
       element.style.visibility = "hidden";
-      element.style.opacity = 1;
+      element.style.opacity = "1";
     }
     setTimeout(() => {
-      element.style.opacity = 0;
+      element.style.opacity = "0";
       element.addEventListener("transitionend", handleTransitionEnd);
     }, TRANSITION_DELAY_MS);
   }
