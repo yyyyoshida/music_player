ae6b95183d13c5ef6b7cd9468aacc230834f5800
yyyyoshida <thth331111@gmail.com>
2025-10-10 14:30:34 +0900

    refactor: SearchContextファイルをts化 #114

--- src/contexts/SearchContext.tsx
+++ src/contexts/SearchContext.tsx
@@ -1,12 +1,27 @@
 import { createContext, useState, useEffect } from "react";
+import type { ReactNode } from "react";
+import type { TrackObject } from "../store/playbackStore";
 import usePlaybackStore from "../store/playbackStore";
 import { useLocation } from "react-router-dom";
 
-export const SearchContext = createContext();
+type SearchContextType = {
+  query: string;
+  setQuery: React.Dispatch<React.SetStateAction<string>>;
+  searchResults: TrackObject[];
+  setSearchResults: React.Dispatch<React.SetStateAction<TrackObject[]>>;
+  hasSearchError: boolean;
+  setHasSearchError: React.Dispatch<React.SetStateAction<boolean>>;
+};
 
-export const SearchProvider = ({ children }) => {
+type SearchContextProviderProps = {
+  children: ReactNode;
+};
+
+export const SearchContext = createContext<SearchContextType | null>(null);
+
+export const SearchProvider = ({ children }: SearchContextProviderProps) => {
   const [query, setQuery] = useState("");
-  const [searchResults, setSearchResults] = useState([]);
+  const [searchResults, setSearchResults] = useState<TrackObject[]>([]);
   const [hasSearchError, setHasSearchError] = useState(false);
   const setQueue = usePlaybackStore((state) => state.setQueue);
   const location = useLocation();
