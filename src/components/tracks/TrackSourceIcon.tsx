9132a74bc03b7823694e315d683773d4f933f85d
yyyyoshida <thth331111@gmail.com>
2025-10-11 11:17:25 +0900

    refactor: TrackSourceIconコンポーネントをts化 #118

--- src/components/tracks/TrackSourceIcon.tsx
+++ src/components/tracks/TrackSourceIcon.tsx
@@ -1,18 +1,21 @@
 import { FaCompactDisc, FaSpotify } from "react-icons/fa";
-import useTooltipStore from "../store/tooltipStore";
+import useTooltipStore from "../../store/tooltipStore";
 
-const TrackSourceIcon = ({ source }) => {
+type TrackSourceIconProps = {
+  source: "spotify" | "local";
+};
+
+const TrackSourceIcon = ({ source }: TrackSourceIconProps) => {
   const setTooltipText = useTooltipStore((state) => state.setTooltipText);
-
   const handleMouseEnter = useTooltipStore((state) => state.handleMouseEnter);
   const handleMouseLeave = useTooltipStore((state) => state.handleMouseLeave);
 
   return (
     <div
       className="track-source-icon-wrapper"
-      onMouseEnter={(e) => {
+      onMouseEnter={() => {
         setTooltipText(source === "spotify" ? "Spotify再生" : "ローカル再生");
-        handleMouseEnter(e);
+        handleMouseEnter();
       }}
       onMouseLeave={() => {
         handleMouseLeave();
