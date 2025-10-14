3c7c4a6677130801ae361c87f15bc5f9b05b8576
yyyyoshida <thth331111@gmail.com>
2025-10-15 06:57:53 +0900

    refactor: playlist-getsファイルをts化 #124

--- server/routes/playlists-get.ts
+++ server/routes/playlists-get.ts
@@ -1,17 +1,21 @@
-const express = require("express");
+import express from "express";
 const router = express.Router();
-const { db } = require("../firebase");
+import { db as firestore } from "../firebase";
 
 // プレイリスト一覧取得
 router.get("/playlists", async (req, res) => {
   try {
-    const playlistsRef = db.collection("playlists").orderBy("createdAt", "asc");
+    const playlistsRef = firestore.collection("playlists").orderBy("createdAt", "asc");
     const playlistSnapshot = await playlistsRef.get();
 
     const playlists = await Promise.all(
       playlistSnapshot.docs.map(async (doc) => {
         const playlistId = doc.id;
-        const tracksSnapshot = await db.collection("playlists").doc(playlistId).collection("tracks").get();
+        const tracksSnapshot = await firestore
+          .collection("playlists")
+          .doc(playlistId)
+          .collection("tracks")
+          .get();
         const tracks = tracksSnapshot.docs.map((doc) => doc.data());
         const tracksSortedByOldest = tracks.slice().sort((a, b) => a.addedAt?.seconds - b.addedAt?.seconds);
         const firstFourAlbumImages = tracksSortedByOldest.slice(0, 4).map((track) => track.albumImage);
@@ -37,9 +41,13 @@ router.get("/playlists", async (req, res) => {
 router.get("/playlists/:id/info", async (req, res) => {
   try {
     const playlistId = req.params.id;
-    const playlistRef = db.collection("playlists").doc(playlistId);
+    const playlistRef = firestore.collection("playlists").doc(playlistId);
     const playlistSnapshot = await playlistRef.get();
-    const data = playlistSnapshot.data();
+
+    if (!playlistSnapshot.exists) {
+      return res.status(404).json({ error: "プレイリストが存在しません" });
+    }
+    const data = playlistSnapshot.data()!;
     const responseData = {
       name: data.name,
       totalDuration: data.totalDuration,
@@ -58,7 +66,11 @@ router.get("/playlists/:id/info", async (req, res) => {
 router.get("/playlists/:id/tracks", async (req, res) => {
   try {
     const playlistId = req.params.id;
-    const tracksRef = db.collection("playlists").doc(playlistId).collection("tracks").orderBy("addedAt", "asc");
+    const tracksRef = firestore
+      .collection("playlists")
+      .doc(playlistId)
+      .collection("tracks")
+      .orderBy("addedAt", "asc");
     const tracksSnapshot = await tracksRef.get();
     const tracks = tracksSnapshot.docs.map((doc) => {
       const data = doc.data();
@@ -77,4 +89,4 @@ router.get("/playlists/:id/tracks", async (req, res) => {
   }
 });
 
-module.exports = router;
+export default router;
