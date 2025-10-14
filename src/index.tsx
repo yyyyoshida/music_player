48e1a0943efc08aacb83ec242500db7b641911f5
yyyyoshida <thth331111@gmail.com>
2025-10-15 05:14:22 +0900

    refactor: indexファイルをts化

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,11 @@
 import { Buffer } from "buffer";
-import ReactDOM from "react-dom/client";
+import { createRoot } from "react-dom/client";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
 window.Buffer = Buffer;
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = createRoot(document.getElementById("root")!);
 root.render(
   // <React.StrictMode>
   // </React.StrictMode>
