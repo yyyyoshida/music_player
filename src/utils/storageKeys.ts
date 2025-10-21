62ef096bdc383935f44eb1b6db70e27630b85e9f
yyyyoshida <thth331111@gmail.com>
2025-10-21 19:41:41 +0900

    chore: ハードコーディングを防ぐためのlocalStorageのキー集を定義するファイルを追加

---
+++ src/utils/storageKeys.ts
@@ -0,0 +1,14 @@
+export const STORAGE_KEYS = {
+  TOKEN: "access_token",
+  REFRESH_TOKEN: "refresh_token",
+  TOKEN_EXPIRY: "access_token_expiry",
+
+  SEARCH_QUERY: "searchQuery",
+  IS_MUTED: "isMuted",
+  VOLUME: "player_volume",
+
+  PLAYLISTS: "playlists",
+
+  getCachedTracksKey: (id: string) => `playlistDetail:${id}Tracks`,
+  getCachedPlaylistInfoKey: (id: string) => `playlistDetail:${id}Info`,
+};
