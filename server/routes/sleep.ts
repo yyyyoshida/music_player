1605e375e8238d9edc5d1f80e67cda09eda234d9
yyyyoshida <thth331111@gmail.com>
2025-10-22 21:39:29 +0900

    feat: Spotify曲をスリープに追加するAPIを実装 #136

---
+++ server/routes/sleep.ts
@@ -0,0 +1,26 @@
+import express from "express";
+const router = express.Router();
+import { admin, db } from "../firebase";
+
+router.post("/sleep/spotify-tracks", async (req, res) => {
+  try {
+    const track = req.body;
+
+    const newTrackRef = await db.collection("sleepTracks").doc(track.id);
+
+    await newTrackRef.set({
+      ...track,
+      addedAt: admin.firestore.FieldValue.serverTimestamp(),
+    });
+
+    const newTrackSnapshot = await newTrackRef.get();
+    const sleepingTrack = { id: newTrackRef.id, ...newTrackSnapshot.data() };
+
+    res.status(200).json({ sleepingTrack });
+  } catch (error) {
+    console.error("曲のスリープに失敗", error);
+    res.status(500).json({ error: "曲のスリープに失敗" });
+  }
+});
+
+export default router;
