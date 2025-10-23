002234bcd83c5f3221e22107ea4e6e9383ebea9a
yyyyoshida <thth331111@gmail.com>
2025-10-23 19:28:33 +0900

    feat(frontend): スリープ曲一覧を動的に取得する処理を実装 #136

--- src/components/SleepTracks.tsx
+++ src/components/SleepTracks.tsx
@@ -1,7 +1,61 @@
-import React from "react";
+import { useEffect } from "react";
 import { pauseIcon, playIcon } from "../assets/icons";
+import usePlaylistStore from "../store/playlistStore";
+import usePlaybackStore from "../store/playbackStore";
+import usePlayerStore from "../store/playerStore";
+import useActionSuccessMessageStore from "../store/actionSuccessMessageStore";
+
+import type { SpotifyTrack, LocalTrack } from "../types/tracksType";
+import useSleepTracks from "../hooks/useSleepTracks";
 
 const SleepTracks = () => {
+  const tracks = usePlaylistStore((state) => state.tracks);
+
+  const { fetchSleepTracks } = useSleepTracks();
+
+  const isPlaying = usePlayerStore((state) => state.isPlaying);
+  const playDisable = usePlayerStore((state) => state.playDisable);
+  const togglePlayPause = usePlayerStore((state) => state.togglePlayPause);
+  const setIsTrackSet = usePlayerStore((state) => state.setIsTrackSet);
+  const currentTrackId = usePlaybackStore((state) => state.currentTrackId);
+  const setCurrentPlayedAt = usePlaybackStore((state) => state.setCurrentPlayedAt);
+  const playTrackAtIndex = usePlaybackStore((state) => state.playTrackAtIndex);
+  const showMessage = useActionSuccessMessageStore((state) => state.showMessage);
+
+  useEffect(() => {
+    fetchSleepTracks();
+  }, []);
+
+  function handleClickTrackItem(track: SpotifyTrack | LocalTrack, date: string, index: number, isCurrentTrack: boolean) {
+    if (playDisable) {
+      showMessage("tooFrequent");
+      return;
+    }
+
+    let uri;
+
+    if ("trackUri" in track) {
+      uri = track.trackUri;
+    } else if ("uri" in track) {
+      uri = track.uri;
+    } else if ("audioURL" in track) {
+      uri = track.audioURL;
+    }
+
+    if (!uri) {
+      return;
+    }
+
+    if (isCurrentTrack) {
+      togglePlayPause();
+      return;
+    }
+
+    setIsTrackSet(true);
+    if (date) setCurrentPlayedAt(date);
+    playTrackAtIndex(index);
+  }
+
   return (
     <div className="sleep">
       <h1 className="sleep__title">スリープ曲一覧</h1>
@@ -9,23 +63,39 @@ const SleepTracks = () => {
       <p className="sleep__text">一時的に非表示にしている曲</p>
 
       <ul className="sleep__track-list">
-        <li className="sleep__track-item">
-          <div className="sleep__track-cover-art-wrapper">
-            <img className="sleep__track-cover-art" src="/img/テストサムネ６.jpg" alt="" />
+        {(tracks as (SpotifyTrack | LocalTrack)[]).map((track, index) => {
+          const addedAt = track.addedAt;
+          const date: string | Date = new Date(addedAt ?? 0).toLocaleString();
 
-            <button className="sleep__track-play-pause-button play-pause-button">
-              <img src={playIcon} alt="" className="sleep__track-play-pause-button-icon play-pause-button-icon play-button-icon" />
-            </button>
-            <button className="sleep__track-restore-button">
-              <img src="img/restore.png" className="sleep__track-restore-button-icon" />
-            </button>
-          </div>
+          const isCurrentTrack = currentTrackId === track.id;
+          const isActiveTrack = currentTrackId === track.id && isPlaying;
 
-          <div className="sleep__track-info">
-            <div className="sleep__track-name">曲のタイトル</div>
-            <div className="sleep__track-artist">アーティスト・作者</div>
-          </div>
-        </li>
+          return (
+            <li
+              className="sleep__track-item"
+              key={track.id}
+              onClick={() => {
+                handleClickTrackItem(track, date, index, isCurrentTrack);
+              }}
+            >
+              <div className="sleep__track-cover-art-wrapper">
+                <img className="sleep__track-cover-art" src={track.albumImage} alt={track.title} />
+
+                <button className="sleep__track-play-pause-button play-pause-button">
+                  <img src={playIcon} alt="" className="sleep__track-play-pause-button-icon play-pause-button-icon play-button-icon" />
+                </button>
+                <button className="sleep__track-restore-button">
+                  <img src="img/restore.png" className="sleep__track-restore-button-icon" />
+                </button>
+              </div>
+
+              <div className="sleep__track-info">
+                <div className="sleep__track-name">{track.title}</div>
+                <div className="sleep__track-artist">{track.artist}</div>
+              </div>
+            </li>
+          );
+        })}
       </ul>
     </div>
   );
