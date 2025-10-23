0cd406b90d8c96a731850a84e727eb22a30557b0
yyyyoshida <thth331111@gmail.com>
2025-10-23 14:20:31 +0900

    feat: スリープ曲一覧を取得するAPIを実装 #136

--- server/routes/sleep.ts
+++ server/routes/sleep.ts
@@ -21,4 +21,24 @@ router.post("/sleep/spotify-tracks", async (req, res) => {
   }
 });
 
+router.get("/sleep/spotify-tracks", async (_req, res) => {
+  try {
+    const sleepTracksRef = db.collection("sleepTracks").orderBy("addedAt", "asc");
+    const sleepTracksSnapshot = await sleepTracksRef.get();
+    const sleepTracks = sleepTracksSnapshot.docs.map((doc) => {
+      const data = doc.data();
+      return {
+        id: doc.id,
+        ...doc.data(),
+        addedAt: data.addedAt ? data.addedAt.toDate().toISOString() : null,
+      };
+    });
+
+    res.status(200).json(sleepTracks);
+  } catch (error) {
+    console.error("エラー", error);
+    res.status(500).json({ error: "トラック取得失敗" });
+  }
+});
+
 export default router;
