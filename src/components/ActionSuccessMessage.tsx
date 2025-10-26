47267e81fcdacc8986357035acc69f76e3a08f8e
yyyyoshida <thth331111@gmail.com>
2025-10-26 22:30:59 +0900

    chore(frontend): Spotify曲の復元の失敗時のメッセージを追加 #138

--- src/components/ActionSuccessMessage.tsx
+++ src/components/ActionSuccessMessage.tsx
@@ -71,6 +71,7 @@ const ActionSuccessMessage = () => {
     sleepFailedSpotify: { text: "Spotify曲のスリープに失敗しました", success: false },
     sleepFailedLocal: { text: "ローカル曲のスリープに失敗しました", success: false },
     sleepTracksFetchFailed: { text: "スリープ曲一覧の取得に失敗しました", success: false },
+    sleepSpotifyRestoreFailed: { text: "Spotify曲をスリープからの復元に失敗しました", success: false },
 
     unselected: { text: "曲がセットされていません", success: false },
     未実装: { text: "この機能はまだ未実装です", success: true },
