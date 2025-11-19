87a8e408310ddbecde255f81783ae8c4c0196b7f
yyyyoshida <thth331111@gmail.com>
2025-11-19 10:34:41 +0900

    chore(test): PrevNextButtonのテストの不要なテストを削除 #157

--- src/components/player/playerTest/PrevNextButton.test.tsx
+++ src/components/player/playerTest/PrevNextButton.test.tsx
@@ -2,87 +2,27 @@ import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import PrevNextButton from "../PrevNextButton";
 import usePlaybackStore from "../../../store/playbackStore";
-import useTooltipStore from "../../../store/tooltipStore";
-
-const mockGoToPreviousTrack = jest.fn();
-const mockGoToNextTrack = jest.fn();
-const mockHandleButtonPress = jest.fn();
-const mockHandleMouseEnter = jest.fn();
-const mockHandleMouseLeave = jest.fn();
-const mockSetTooltipText = jest.fn();
 
 jest.mock("../../../store/playbackStore");
-jest.mock("../../../store/tooltipStore");
-
-beforeEach(() => {
-  jest.clearAllMocks();
-
-  (usePlaybackStore as unknown as jest.Mock).mockImplementation((selector) =>
-    selector({
-      goToPreviousTrack: mockGoToPreviousTrack,
-      goToNextTrack: mockGoToNextTrack,
-      isPrevDisabled: false,
-      isNextDisabled: false,
-    })
-  );
-
-  (useTooltipStore as unknown as jest.Mock).mockImplementation((selector) =>
-    selector({
-      handleButtonPress: mockHandleButtonPress,
-      handleMouseEnter: mockHandleMouseEnter,
-      handleMouseLeave: mockHandleMouseLeave,
-      setTooltipText: mockSetTooltipText,
-    })
-  );
-});
 
 describe("PrevNextButton", () => {
-  test("Prevボタンのクリックとツールチップ呼び出し確認", async () => {
-    render(<PrevNextButton type="prev" />);
-
-    const button = screen.getByRole("button");
-    expect(button).toBeInTheDocument();
-
-    await userEvent.click(button);
-    expect(mockHandleButtonPress).toHaveBeenCalledTimes(1);
-    expect(mockGoToPreviousTrack).toHaveBeenCalledTimes(1);
-
-    await userEvent.hover(button);
-    expect(mockSetTooltipText).toHaveBeenCalledWith("前へ");
-    expect(mockHandleMouseEnter).toHaveBeenCalled();
-
-    await userEvent.unhover(button);
-    expect(mockHandleMouseLeave).toHaveBeenCalled();
-  });
-
-  test("Nextボタンのクリックとツールチップ呼び出し確認", async () => {
-    render(<PrevNextButton type="next" />);
-
-    const button = screen.getByRole("button");
-    expect(button).toBeInTheDocument();
-
-    await userEvent.click(button);
-    expect(mockHandleButtonPress).toHaveBeenCalledTimes(1);
-    expect(mockGoToNextTrack).toHaveBeenCalledTimes(1);
-
-    await userEvent.hover(button);
-    expect(mockSetTooltipText).toHaveBeenCalledWith("次へ");
-    expect(mockHandleMouseEnter).toHaveBeenCalled();
-
-    await userEvent.unhover(button);
-    expect(mockHandleMouseLeave).toHaveBeenCalled();
-  });
+  const mockGoToPreviousTrack = jest.fn();
+  const mockGoToNextTrack = jest.fn();
 
   beforeEach(() => {
-    mockGoToPreviousTrack.mockClear();
-    mockGoToNextTrack.mockClear();
-    mockHandleButtonPress.mockClear();
-    mockHandleMouseEnter.mockClear();
-    mockHandleMouseLeave.mockClear();
-    mockSetTooltipText.mockClear();
+    jest.clearAllMocks();
+
+    (usePlaybackStore as unknown as jest.Mock).mockImplementation((selector) =>
+      selector({
+        goToPreviousTrack: mockGoToPreviousTrack,
+        goToNextTrack: mockGoToNextTrack,
+        isPrevDisabled: false,
+        isNextDisabled: false,
+      })
+    );
   });
 
-  test("前へボタン無効化テスト", async () => {
+  test("前ボタンが無効時は処理が呼ばれない", async () => {
     (usePlaybackStore as unknown as jest.Mock).mockImplementation((selector) =>
       selector({
         goToPreviousTrack: mockGoToPreviousTrack,
@@ -91,16 +31,15 @@ describe("PrevNextButton", () => {
         isNextDisabled: false,
       })
     );
+
     render(<PrevNextButton type="prev" />);
-
     const button = screen.getByRole("button");
-    expect(button).toHaveClass("disabled");
 
-    await userEvent.click(button);
+    userEvent.click(button);
     expect(mockGoToPreviousTrack).not.toHaveBeenCalled();
   });
 
-  test("Nextボタン無効化テスト", async () => {
+  test("次ボタンが無効時は処理が呼ばれない", async () => {
     (usePlaybackStore as unknown as jest.Mock).mockImplementation((selector) =>
       selector({
         goToPreviousTrack: mockGoToPreviousTrack,
@@ -112,9 +51,8 @@ describe("PrevNextButton", () => {
 
     render(<PrevNextButton type="next" />);
     const button = screen.getByRole("button");
-    expect(button).toHaveClass("disabled");
 
-    await userEvent.click(button);
+    userEvent.click(button);
     expect(mockGoToNextTrack).not.toHaveBeenCalled();
   });
 });
