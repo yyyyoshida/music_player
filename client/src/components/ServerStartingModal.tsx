670b7c248223bf4a6a39896bcb795ae0c7ed08b8
yyyyoshida <thth331111@gmail.com>
2025-12-23 09:36:15 +0900

    fix(frontend): サーバー起動中モーダルが描画されない＆不要なコードを削除 #177

--- client/src/components/ServerStartingModal.tsx
+++ client/src/components/ServerStartingModal.tsx
@@ -1,24 +1,9 @@
-import { useEffect, useState } from "react";
-import LOGIN_URL from "../config/spotifyConfig";
-import useTokenStore from "../store/tokenStore";
-
 const ServerStartingModal = () => {
-  const isServerStatus = useTokenStore((state) => state.serverStatus);
-  const isServerReady = isServerStatus === "ready";
-
-  const handleLogin = () => {
-    window.location.href = LOGIN_URL; // Spotifyの認証ページにリダイレクト
-  };
-
-  // LoginではなくServerStartingModalとかに名前変える
-
   return (
-    <div className="server-starting-modal modal" style={{ visibility: !isServerReady ? "hidden" : "visible" }}>
+    <div className="server-starting-modal modal">
       <div className="server-starting-modal__smoke modal-smoke"></div>
       <div className="server-starting-modal__content modal-content">
-        {/* {`${isServerStatus}`} */}
         <div className="server-starting-modal__spin-loader spin-loader"></div>
-        {/* <h2 className="server-starting-modal__title modal-title">サーバー起動中</h2> */}
         <p className="server-starting-modal__title modal-text">サーバー起動中</p>
       </div>
     </div>
